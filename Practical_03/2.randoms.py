25a70f60aaf7f702e49ba1955f810a1e2d54114c
asmitasharma01 <128336210+asmitasharma01@users.noreply.github.com>
2023-04-24 14:57:50 +0800

    Add files via upload

---
+++ Practical_03/2.randoms.py
@@ -0,0 +1,8 @@
+import random
+
+"""The smallest number I could have seen was 5 and the largest number was 20"""
+"""The smallest number I could have gotten was 3 and the largest was 9, I would not be able to get a 4 since the start 
+is 3 and the steps the random number could generate was 2"""
+"""The smallest number I could have seen was 2.5 while the largest number I could have seen was 5.5"""
+random_num = random.randint(1, 100)
+print(random_num)
