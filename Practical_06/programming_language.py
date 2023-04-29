775bfc31914853a16c7e1868850d29b1cb908c55
asmitasharma01 <128336210+asmitasharma01@users.noreply.github.com>
2023-04-29 22:46:30 +0800

    Add files via upload

---
+++ Practical_06/programming_language.py
@@ -0,0 +1,20 @@
+"""
+Estimated time : 30 minutes
+Actual time : 10 minutes
+"""
+
+class ProgrammingLanguage:
+    def __init__(self, name="", typing="", reflection=bool, year=0):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} typing, Reflection = {self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        if self.typing == "Dynamic":
+            return True
+        else:
+            return False
